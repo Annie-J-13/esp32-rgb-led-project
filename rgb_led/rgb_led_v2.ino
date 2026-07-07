a3aca621c6ddd0feef887cca1571b2cc79664ac9
Ann Joshua <annmariejoshua13@gmail.com>
2026-07-07 22:18:30 +0100

    Create rgb_led_v2.ino
    Uploaded improved code

---
+++ rgb_led/rgb_led_v2.ino
@@ -0,0 +1,82 @@
+//RGB LED circuit
+
+//Define Pins
+#define BLUE 5
+#define GREEN 4
+#define RED 2
+#define delayTime 10 // fading time between colours
+
+void setup() {
+pinMode(RED, OUTPUT);
+pinMode(GREEN, OUTPUT);
+pinMode(BLUE, OUTPUT);
+digitalWrite(RED, HIGH);
+digitalWrite(GREEN, LOW);
+digitalWrite(BLUE, LOW);
+}
+
+// Define Variables
+int redValue;
+int greenValue;
+int blueValue;
+
+// main loop
+void loop(){
+
+redValue = 255; //choose a value between 1 and 255 to change the colour
+greenValue = 0; 
+blueValue = 0;
+
+// the above command is unnecessary as we have already turned on RED in SETUP
+// or in the previous loop... regardless, this turns RED off
+// analogWrite(RED, 0);
+// delay(1000);
+
+for (int i = 0; i < 255; i ++ ) // fades out RED bring GREEN full when i=255
+{
+redValue -= 1;
+greenValue += 1;
+// The following was reversed, counting in the wrong directions
+// analogWrite(RED, 255 - redValue);
+// analogWrite(GREEN, 255 - greenValue);
+analogWrite(RED, redValue);
+analogWrite(GREEN, greenValue);
+
+delay(delayTime);
+}
+
+redValue = 0;
+greenValue = 255;
+blueValue = 0;
+
+for(int i = 0; i < 255; i ++ ) // fades out GREEN bring BLUE full when i=255
+{
+greenValue -= 1;
+blueValue += 1;
+// The following was reversed, counting in the wrong directions
+// analogWrite(GREEN, 255 - greenValue);
+// analogWrite(BLUE, 255 - blueValue);
+analogWrite(GREEN, greenValue);
+analogWrite(BLUE, blueValue);
+
+delay(delayTime);
+}
+
+redValue = 0;
+greenValue = 0;
+blueValue = 255;
+
+for (int i = 0; i < 255; i ++ ) // fades out BLUE bring RED full when i=255
+{
+// The following code has been rearranged to match the other two similar sections
+blueValue -= 1;
+redValue += 1;
+// The following was reversed, counting in the wrong directions
+// analogWrite(BLUE, 255 - blueValue);
+// analogWrite(RED, 255 - redValue);
+analogWrite(BLUE, blueValue);
+analogWrite(RED, redValue);
+
+delay(delayTime);
+}
+}
